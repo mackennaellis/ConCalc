f80f873a28ed1ed1c33cf362cbcd39ac7312310c
mackennaellis <ellism@stu.otc.school.nz>
2024-06-11 10:29:10 +1200

    Renamed "ConCalc operations (code here the copy and paste" to "Volume Calculations". This is the working and 'complete' version of this option.

--- Volume Calculations.py
+++ Volume Calculations.py
@@ -1,3 +1,5 @@
+import math
+import os
 
 def enter_line():                   #to leave an 'enter' space between lines 
     print(' ')                       #to make it easier for users to read
@@ -16,56 +18,64 @@ print('4. Cone')
 print('5. Sphere')
 enter_line()
 
-import math
+#needed math imports:
 math.pi == math.pi
 
 while True:
     what_shape = input("What shape's volume would you like to calculate from the list? ")
-    enter_line()
+    os.system('cls')
     if what_shape == '1':
         print('☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲')
+        enter_line()
         print('\033[1m' + 'Cuboid volume selected: ')
         enter_line()
-        height = int(input('\033[0m' + 'What is the height of the cuboid? '))
+        height = float(input('\033[0m' + 'What is the height of the cuboid? '))
         enter_line()
-        width = int(input('What is the width of the cuboid? '))
+        width = float(input('What is the width of the cuboid? '))
         enter_line()
-        depth = int(input('What is the depth of the cuboid? '))
+        depth = float(input('What is the depth of the cuboid? '))
         enter_line()
         print('☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲')
-        enter_line()
+        os.system('cls')
         print('☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲')
         enter_line()
         print('\033[1m' + 'Cuboid volume selected: ')
         enter_line()
         
         cuboid_volume = height * width * depth
-        print(f'\033[0m' + "Your cuboid's volume is: " '\033[1m' f'{cuboid_volume}')
+        cuboid_volume_rounded = round(cuboid_volume, 3)
+
+        print(f'\033[0m' + "Your cuboid's volume is: " '\033[1m' f'{cuboid_volume_rounded}')
         enter_line()
         print('☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲')
 
         break                               #'break' stops the 'continue' and prevents i from repeating infinitily
     elif what_shape == '2':
         print('☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲')
+        enter_line()
         print('\033[1m' + 'Cube volume selected: ')
         enter_line()
-        measurement = int(input('\033[0m' + 'What is the side measurement of the cuboid? '))
+        measurement = float(input('\033[0m' + 'What is the side measurement of the cube? '))
         enter_line()
       
         print('☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲')
-        enter_line()
+        os.system('cls')
         print('☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲')
         enter_line()
         print('\033[1m' + 'Cube volume selected: ')
         enter_line()
         
         cube_volume = measurement * 3
-        print(f'\033[0m' + "Your cube's volume is: " '\033[1m' f'{cube_volume}')
+        cube_volume_rounded = round(cube_volume, 3)
+
+        print(f'\033[0m' + "Your cube's volume is: " '\033[1m' f'{cube_volume_rounded}')
+        enter_line()
         print('☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲')
         enter_line()
         break
     elif what_shape == '3':
         print('☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲')
+        enter_line()
         print('\033[1m' + 'Cylinder volume selected: ')
         enter_line()
         height = float(input('\033[0m' + 'What is the height of the cylinder? '))
@@ -73,7 +83,7 @@ while True:
         radius = float(input('What is the radius of the cylinder? '))
         enter_line()
         print('☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲')
-        enter_line()
+        os.system('cls')
         print('☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲')
         enter_line()
         print('\033[1m' + 'Cylinder volume selected: ')
@@ -82,17 +92,56 @@ while True:
         cylinder_volume = math.pi * (radius ** 2) * height
         cylinder_volume_rounded = round(cylinder_volume, 3)
 
-        print(f'\033[0m' + "Your cuboid's volume is: " '\033[1m' f'{cylinder_volume_rounded}')
+        print(f'\033[0m' + "Your cylinders's volume is: " '\033[1m' f'{cylinder_volume_rounded}')
         enter_line()
         print('☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲')
         break
     elif what_shape == '4':
-        print('holder')
+        print('☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲')
+        enter_line()       
+        print('\033[1m' + 'Cone volume selected: ')
+        enter_line()
+        height = float(input('\033[0m' + 'What is the height of the cone? '))
+        enter_line()
+        radius = float(input('What is the radius of the cone? '))
+        enter_line()
+        print('☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲')
+        os.system('cls')
+        print('☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲')
+        enter_line()
+        print('\033[1m' + 'Cone volume selected: ')
+        enter_line()
+        
+        cone_volume = math.pi * (radius ** 2) * (height/3)
+        cone_volume_rounded = round(cone_volume, 3)
+
+        print(f'\033[0m' + "Your cones's volume is: " '\033[1m' f'{cone_volume_rounded}')
+        enter_line()
+        print('☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲')
         break
     elif what_shape == '5':
-        print('placeholder')
+        print('☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲')
+        enter_line()
+        print('\033[1m' + 'Sphere volume selected: ')
+        enter_line()
+        radius = float(input('What is the radius of the sphere? '))
+        enter_line()
+        print('☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲')
+        os.system('cls')
+        print('☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲')
+        enter_line()
+        print('\033[1m' + 'Sphere volume selected: ')
+        enter_line()
+        
+        sphere_volume = (4/3) * math.pi * (radius ** 3)
+        sphere_volume_rounded = round(sphere_volume, 3)
+
+        print(f'\033[0m' + "Your sphere's volume is: " '\033[1m' f'{sphere_volume_rounded}')
+        enter_line()
+        print('☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲')
         break
     else:
+        print('☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲')
         enter_line()
         print('Invalid input, try a number between 1 and 5')
         enter_line()
