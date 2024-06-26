2283923385b89279c0c9f0ffc6fa17368d851df1
mackennaellis <ellism@stu.otc.school.nz>
2024-06-26 15:00:53 +1200

    another calc - need to ask Mrs. Strauss how to put into other statements but continue to outter one Basic calcs - need to ask Mrs. Strauss why 'if' prints aren't working?! Welcome - Just to save

--- Basic Calculations.py
+++ Basic Calculations.py
@@ -23,13 +23,59 @@ while True:
     os.system('cls')
 
     if basic_calc == '1':
-        numerical_to_add = []
+        numericals = []
 
         print('☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲')
         enter_line()
+        while True:
+            number = input("What is the first number you'd like to add? ")
+            os.system('cls')
+            if number > '0':
+                number = float
+                numericals.append(number)
+                os.system('cls')
+                break
+            elif number == 'None': #how to get this to print what it's ment to?!
+                print('☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲')
+                enter_line()
+                print('Thank you for usuing the program!!')
+                enter_line()
+                another_calculation = input('Would you like to complete another calculation?')
+                break
+            else:
+                print('Invaild input, try a number larger then 1')
+                enter_line()
+                continue
+            
         
+        while True:
+            print('☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲')
+            enter_line()
+            next_number = input("What is the next number you'd like to add? ")
+            os.system('cls')
+            if next_number > '0':
+                next_number = float
+                numericals.append(next_number)
+                continue
 
-        break
+            elif next_number == 'None': #how to get this to print what it's ment to?!
+                print('☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲')
+                enter_line()
+                numericals.sort()
+                print(f"The addition of your {len(numericals)} values is equal to : " + "\\033[1m" + f'{sum(numericals)}')
+                enter_line()
+                print('☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲')
+                break
+
+            else:
+                print('☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲☲')
+                enter_line()
+                print('Invaild input, try a number larger then 1')
+                enter_line()
+                continue
+
+    
+
     elif basic_calc == '2':
         print(':)')
         break
